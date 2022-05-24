a9b0b4d8b67f524c7fe0879dfad552d8c0978b8d
kino <66652533+kino-ona@users.noreply.github.com>
2022-05-24 21:39:24 +0900

    페이지 연결 링크 및 수정사항

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,5 +1,14 @@
 'use strict';
 
+/* iphone scroll bug */
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 /* accordion */
 
 class Accordion {
@@ -37,3 +46,63 @@ class Accordion {
 
 const accordion = new Accordion('.accordion__header');
 accordion.allowMulti();
+
+
+
+const spinnerBar = `
+<div class="spinner">
+<div class="spinner__circle">
+<div class="spinner__bar"></div>
+<div class="spinner__bar"></div>
+<div class="spinner__bar"></div>
+<div class="spinner__bar"></div>
+<div class="spinner__bar"></div>
+<div class="spinner__bar"></div>
+<div class="spinner__bar"></div>
+<div class="spinner__bar"></div>
+<div class="spinner__bar"></div>
+<div class="spinner__bar"></div>
+<div class="spinner__bar"></div>
+<div class="spinner__bar"></div>
+</div></div>
+<p>
+`;
+function loadingFn() {
+const element = document.getElementById('spinner');
+element.innerHTML = spinnerBar;
+} 
+
+document.querySelectorAll('.button-radius').forEach((item) => {
+	item.addEventListener('click', () => {
+		item.nextElementSibling.style.display = "block";
+	})
+})
+
+// let cookiedata = document.cookie; 
+// if ( cookiedata.indexOf("ncookie=done") < 0 ){ 
+// 	document.getElementById('popup').style.display = "block";    //  팝업창 아이디
+// } else {
+// 	document.getElementById('popup').style.display = "none";    // 팝업창 아이디
+// }
+
+
+function setCookie( name, value, expiredays ) { 
+	let todayDate = new Date(); 
+	todayDate.setDate( todayDate.getDate() + expiredays );
+	document.cookie = name + "=" + escape( value ) + "; path=/; expires=" + todayDate.toGMTString() + ";"
+}
+
+
+function closeWin() { 
+	document.querySelectorAll('#modal').forEach((item) => {
+		item.style.display = "none";
+	})   
+}
+
+function todaycloseWin() { 
+	setCookie( "ncookie", "done" , 1 );     // 저장될 쿠키명 , 쿠키 value값 , 기간( ex. 1은 하루, 7은 일주일)
+	document.querySelectorAll('#modal').forEach((item) => {
+		item.style.display = "none";
+	})
+
+}
